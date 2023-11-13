b7aeb8474409c4c7a3abfb828007f96fbcc3dfb6
rootnure <nur.diu.2791@gmail.com>
2023-11-13 17:04:17 +0600

    Category added to home using Swiper (React Awesome Components)

---
+++ src/pages/Home/Category.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+import categoryImg1 from "../../assets/home/slide1.jpg";
+import categoryImg2 from "../../assets/home/slide2.jpg";
+import categoryImg3 from "../../assets/home/slide3.jpg";
+import categoryImg4 from "../../assets/home/slide4.jpg";
+import categoryImg5 from "../../assets/home/slide5.jpg";
+
+const Category = () => {
+    return (
+        <>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={10}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={categoryImg1} alt="" className='w-fit mx-auto' />
+                    <h3 className='text-4xl uppercase text-center -mt-12 text-white font-bold [text-shadow:_4px_4px_0_rgb(0_0_0_/_40%)]'>Salads</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={categoryImg2} alt="" className='w-fit mx-auto' />
+                    <h3 className='text-4xl uppercase text-center -mt-12 text-white font-bold [text-shadow:_4px_4px_0_rgb(0_0_0_/_40%)]'>Pizzas</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={categoryImg3} alt="" className='w-fit mx-auto' />
+                    <h3 className='text-4xl uppercase text-center -mt-12 text-white font-bold [text-shadow:_4px_4px_0_rgb(0_0_0_/_40%)]'>Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={categoryImg4} alt="" className='w-fit mx-auto' />
+                    <h3 className='text-4xl uppercase text-center -mt-12 text-white font-bold [text-shadow:_4px_4px_0_rgb(0_0_0_/_40%)]'>Dessert</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={categoryImg5} alt="" className='w-fit mx-auto' />
+                    <h3 className='text-4xl uppercase text-center -mt-12 text-white font-bold [text-shadow:_4px_4px_0_rgb(0_0_0_/_40%)]'>Salads</h3>
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Category;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Container from "../../components/Container";
 import Banner from "./Banner";
+import Category from "./Category";
 
 const Home = () => {
     return (
-        <div>
+        <div className="space-y-24 mb-24">
             <Container>
                 <Banner></Banner>
+            </Container>
+            <Container>
+                <Category></Category>
             </Container>
         </div>
     );
